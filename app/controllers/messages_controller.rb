17a50db4e94e6c1789271ea120a0e6f0a818abe4
関川智 <t.g.i.f.0312@gmail.com>
2018-05-05 19:11:56 +0900

    edit_message-controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,14 @@
 class MessagesController < ApplicationController
-
   def index
+    @message = Message.find
+  end
+
+  def create
+    @message = Message.new(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:group).permit(:body, :image, :user 'current_user', :group)
   end
 end
