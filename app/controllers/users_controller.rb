02a52bb02f37be144634293c59dc1ce4d3681b1f
関川智 <t.g.i.f.0312@gmail.com>
2018-05-15 20:31:58 +0900

    add_user-controller_index-action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
     respond_to do |format|
       format.html
       format.json
