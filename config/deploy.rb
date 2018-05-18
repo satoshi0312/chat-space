6ac065fa6b4ad1cced2fb2eae7f4fbc2823b742c
関川智 <t.g.i.f.0312@gmail.com>
2018-05-18 19:02:33 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '~> 3.10.2'
 
 set :application, 'chat-space'
-set :repo_url,  'git@example.com:satoshi0314/chat-space.git'
+set :repo_url,  'https://github.com/satoshi0314/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
