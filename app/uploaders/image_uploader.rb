0010c9f6565b95d35a3583af4a76b36ae4ef8122
関川智 <t.g.i.f.0312@gmail.com>
2018-05-19 17:22:19 +0900

    comment-out_CarrierWaveMiniMagick

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,9 +4,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  # storage :file
   storage :fog
-  # storage :fog
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
   process resize_to_fit: [700, 700]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
